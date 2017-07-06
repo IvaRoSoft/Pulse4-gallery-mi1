34e8f5a8c828904fe7e7811df7915d5375b2dff4
IvaRo <ivarosoft@gmail.com>
2017-07-06 09:53:21 +0200

    Add files via upload

---
+++ gal-mi1.php
@@ -0,0 +1,72 @@
+<style>
+.<?php echo $tag_var2; ?> img {
+	margin-right: 20px;
+	margin-bottom: 20px;
+	height: <?php echo $th_height = $tag_var3 ?>px;
+}
+</style>
+
+<link rel="stylesheet" href="<?php echo $path; ?>/inc/tags/css/magnific-popup.css">
+<script src="<?php echo $path; ?>/inc/plugins/lightbox/jquery.magnific-popup.min.js"></script>
+
+<div class="<?php echo $tag_var2; ?>">
+
+<?php
+$galdir = $tag_var1;
+$images = glob("content/media/$galdir/*");
+$taken  = array();
+$path_new = '';
+if (!empty($path)) { $path_new = $path.'/'; }
+
+$opFile = "content/media/". $galdir ."/gallery.txt";
+
+if (file_exists($opFile)) {
+	$fp          = fopen($opFile,"r");
+	$data        = @fread($fp, filesize($opFile));
+	fclose($fp);
+	$line        = explode("\n", $data);
+
+
+	foreach($line as $test){
+		if(!empty($test)){
+    		$test_line[] = explode("|", $test);
+		}
+	}
+
+	foreach ($test_line as $t){
+		$image = "content/media/$galdir/".$t[0];
+		$info  = pathinfo($image);
+		$ext   = $info['extension'];
+		if ($ext != 'txt' || empty($ext)){
+    		$taken[] = $image;
+			echo "<a title=\"$t[2]\" href='$path_new$image'><img src='$path_new$image'></a>";
+		}
+	}
+}
+
+foreach ($images as $image) {
+    if (!in_array($image, $taken)){
+    	$info   = pathinfo($image);
+		$ext    = $info['extension'];
+		if ($ext != 'txt'){
+	    	echo "<a title='' href='$path_new$image'><img src='$path_new$image'></a>";
+		}
+    }
+}
+
+unset($galdir,$images,$test_line,$tag_var1,$image,$opFile,$line,$fp,$data,$test,$taken);
+?>
+
+</div>
+
+<script>
+$('.<?php echo $tag_var2; ?>').each(function() {
+    $(this).magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        disableOn: 10,
+        gallery:{enabled:true}
+    });
+});
+</script>
+<?php unset( $tag_var2,$tag_var3,$th_height); ?>
